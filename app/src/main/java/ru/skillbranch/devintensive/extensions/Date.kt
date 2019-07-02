d85aade9a25b710a60f4a2ecfea0b54b8076e11a
m.kravchenko <marinakravchenko@itomy.ch>
2019-07-02 14:33:55 +0300

    date

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -8,20 +8,21 @@ const val MINUTE = 60* SECONDS
 const val HOUR = 60* MINUTE
 const val DAY = 24* HOUR
 
-fun Date.format(pattern: String="HH.mm.ss.yy"): String{
+fun Date.format(pattern: String="HH.mm.ss.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
-
+}
 
     fun Date.add (value:Int, units:String):Date {
         var time = this.time
 
-        when (units) {
+        time +=when (units) {
             "second" , "seconds" -> value * SECONDS
             "minute" , "minutes" -> value * MINUTE
             "hour" , "hours" -> value * HOUR
             "day" , "days" -> value * DAY
             else -> throw IllegalStateException("invalid unit")
         }
+        this.time = time
+        return this
     }
-}
