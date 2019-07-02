3bf7c168894bf9b9ebe9ed4310e1d56d576403ab
Alex Anurin <Anurin.Alexey@gmail.com>
2019-07-02 18:16:31 +0300

    } fix

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -4,26 +4,25 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 const val SECONDS = 1000L
-const val MINUTE = 60* SECONDS
-const val HOUR = 60* MINUTE
-const val DAY = 24* HOUR
+const val MINUTE = 60 * SECONDS
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
 
-fun Date.format(pattern: String="HH.mm.ss.yy"): String{
+fun Date.format(pattern: String = "HH.mm.ss.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
 }
 
-    fun Date.add (value:Int, TimeUnits:String):Date {
-        var time = this.time
+fun Date.add(value: Int, TimeUnits: String): Date {
+    var time = this.time
 
-        when (TimeUnits) {
-            "second" , "seconds" -> value * SECONDS
-            "minute" , "minutes" -> value * MINUTE
-            "hour" , "hours" -> value * HOUR
-            "day" , "days" -> value * DAY
-            else -> throw IllegalStateException("invalid unit")
-        }
-this.time=time
-        return this
+    when (TimeUnits) {
+        "second", "seconds" -> value * SECONDS
+        "minute", "minutes" -> value * MINUTE
+        "hour", "hours" -> value * HOUR
+        "day", "days" -> value * DAY
+        else -> throw IllegalStateException("invalid unit")
     }
+    this.time = time
+    return this
 }
