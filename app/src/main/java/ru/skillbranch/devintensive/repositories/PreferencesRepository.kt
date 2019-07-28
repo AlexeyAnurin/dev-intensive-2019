4d240a31e74153208b29056233cfff955a8ce8bd
Alex Anurin <Anurin.Alexey@gmail.com>
2019-07-28 16:02:29 +0300

    Lektion 4. Vorbereitung

--- app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.repositories
 
+import ru.skillbranch.devintensive.models.Profile
+
 //object в Kotlin - это SingleTon. Экземпляр в единственном числе, ленивая инициализация
 object PreferencesRepository {
 
